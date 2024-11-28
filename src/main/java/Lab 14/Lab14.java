e359ef8bb839e2aebe161f3af19b34219aad0223
JaehoonSong12 <manual20151276@gmail.com>
2024-11-28 10:04:14 -0500

    quick_mod

---
+++ src/main/java/Lab 14/Lab14.java
@@ -0,0 +1,55 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Circle;
+
+public class Lab14 extends Application
+{
+   public static void main(String[] args)
+   {
+      launch(args);
+   }
+   
+   @Override
+   public void start(Stage primaryStage)
+   {
+      // Constants for the scene size
+      final double SCENE_WIDTH = 520.0;
+      final double SCENE_HEIGHT = 520.0;
+      
+      // Constants for each square's XY coordinates
+      final int X1 = 10, Y1 = 10;      // Square #1
+      final int X2 = 60, Y2 = 60;      // Square #1
+      final int X3 = 110, Y3 = 110;    // Square #3
+      
+      // Constants for each square's width and height
+      final int WIDTH1 = 500, HEIGHT1 = 500; // Square #1
+      final int WIDTH2 = 400, HEIGHT2 = 400; // Square #2
+      final int WIDTH3 = 300, HEIGHT3 = 300; // Square #3
+      
+      // Constants for the circle's geometry
+      final int CENTER_X = 260, CENTER_Y = 260, RADIUS = 150
+      
+      // Create square #1 here. Set its stroke color to black
+      // and set its fill color to null.
+      
+      // Create square #2 here. Set its stroke color to black
+      // and set its fill color to null.
+      
+      // Create square #3 here. Set its stroke color to black
+      // and set its fill color to null.
+      
+      // Create the diagonal lines here.
+      
+      // Create the circle here.
+      
+      // Add the nodes to a Pane here.
+      
+      // Create a Scene with the Pane as the root node,
+      // and display it here.
+   }
+}
