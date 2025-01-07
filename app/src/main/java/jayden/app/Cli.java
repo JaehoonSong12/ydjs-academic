364eb51c54932689759c888ba990d915d57db28e
okggff <imageai317@gmail.com>
2025-01-06 20:32:02 -0500

    Commit from  on Mon, Jan  6, 2025  8:32:00 PM

--- app/src/main/java/jayden/app/Cli.java
+++ app/src/main/java/jayden/app/Cli.java
@@ -2,11 +2,50 @@ package jayden.app;
 
 import jayden.unit.a.Exercise;
 
+import javax.swing.JOptionPane; // Needed for JOptionPane
+
 public class Cli {
     public static void main(String[] args) {
         System.out.println("Hello, Cli!");
         System.out.println("This is just for testing!");
 
         Exercise.main(args); // test homework! Unit A
+
+
+
+        test();
+    }
+
+    public static void test() {
+        double score1; // To hold score #1
+        double score2; // To hold score #2
+        double score3; // To hold score #3
+        double average; // To hold the average score
+        String input; // To hold the user's input
+
+        // Get the first test score.
+        input = JOptionPane.showInputDialog("Enter score #1:");
+        score1 = Double.parseDouble(input);
+
+        // Get the second score.
+        input = JOptionPane.showInputDialog("Enter score #2:");
+        score2 = Double.parseDouble(input);
+
+        // Get the third test score.
+        input = JOptionPane.showInputDialog("Enter score #3:");
+        score3 = Double.parseDouble(input);
+
+        // Calculate the average score.
+        average = (score1 + score2 + score3) / 3.0;
+
+        // Display the average score.
+        JOptionPane.showMessageDialog(null, "The average is " + average);
+
+        // If the average was greater than 95, let the user know that's a great score.
+        if (average > 95) {
+            JOptionPane.showMessageDialog(null, "That's a great score!");
+        }
+
+        System.exit(0);
     }
 }
