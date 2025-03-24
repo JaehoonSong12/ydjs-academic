0576f703da1f33df5384aa768bde580c6fb63e58
JaehoonSong12 <manual20151276@gmail.com>
2025-03-24 18:57:14 -0400

    Normal Execution commit: 2025-03-24_18-57-14 updated by Jaehoon

--- async-jayden/facade_practice.py
+++ async-jayden/facade_practice.py
@@ -1,5 +1,23 @@
+"""
+Description:
+    This script demonstrates basic file I/O operations in Python.
+    It performs two main tasks:
+      1. Writes two lines of text to a file located at "async-jayden/sample.txt".
+      2. Reads the contents of the file and prints it to the console.
+      
+    The script is designed to illustrate the following concepts:
+      - How to open a file for writing and add content to it.
+      - The importance of properly closing files to free system resources.
+      - How to open a file for reading, process its content, and handle potential exceptions using try-finally.
+      
+Instructions to run the tests via the CLI:
+    1. Open your terminal or command prompt from the root directory.
+    2. Execute the tests by running: `python async-jayden/facade_practice.py`
+"""
+
+
 # Open file in write mode
-f = open("sample.txt", "w")
+f = open("async-jayden/sample.txt", "w")
 f.write("This is the first line.\n")
 f.write("This is the second line.\n")
 # Manually close the file
@@ -8,7 +26,7 @@ f.close()
 
 
 # Open file in read mode
-f = open("sample.txt", "r")
+f = open("async-jayden/sample.txt", "r")
 try:
     content = f.read()
     print("File content:")
--- script/bashrc-init.sh
+++ script/bashrc-init.sh
@@ -521,6 +521,72 @@ function instantiate_bash {
 
 #
 
+###############################################################################
+#                                   ?PYTHON;
+################################################################################
+# PVM="/Python312"                    # Python interpreter (Virtual Machine)
+# TKI="/Python312/libs"               # tkinter (TK graphical user Interface)
+# PXE="/Python312/Scripts"            # Python eXecutablE scripts, e.g. pip (dependency management)
+# PIL="/Python312/Lib"                # Python Internal Library (standard)
+# PXL="/Python312/Lib/site-packages"  # Python External Library (3rd party packages)
+#################################< VERSION >####################################
+PYTHON_VERSIONS=("3.8.10" "3.9.13" "3.10.11" "3.11.2" "3.12.2")
+PYTHON_VERSIONS=("3.10.11" "3.11.2" "3.12.2")
+PYTHON_VERSIONS=("3.12.2")
+##################################< USAGE >#####################################
+# ### language system setup ... (1)
+# on_pvm
+###############################################################################
+PYTHON_URL="https://www.python.org/ftp/python/{VERSION}/python-{VERSION}-embed-amd64.zip"
+PYTHON_DIR="${PROJ_EXT}/Python{VERSION}"
+function on_pvm {
+    # PVM checkup (positive)
+    if exists "$PROJ_EXT" "Python"; then
+        echo "Notice: Directory $(USRPROMPT ''${PYTHON_DIR}' ')already exists. PVE has been setup..."
+        # PVE setup
+        SETUP_PYTHON
+        return
+    fi
+    # PVM install
+    for ver in "${PYTHON_VERSIONS[@]}"; do
+        url="${PYTHON_URL//\{VERSION\}/$ver}"
+        echo $url
+        ver=$(echo "$ver" | awk -F. '{print $1 $2}')                                        # version modification e.g. "3.12.2" to "312"
+        INSTRACT "$url" "python.zip" "${PROJ_EXT}/Python$ver"
+        echo -e "python$ver.zip\n.\nimport site" > "$PROJ_EXT/Python$ver/python$ver._pth"   # enable `Lib/site-packages`
+        curl -o "${PROJ_EXT}/get-pip.py" "https://bootstrap.pypa.io/get-pip.py"             # PIP script
+        ${PYTHON_DIR//\{VERSION\}/$ver}/python "${PROJ_EXT}/get-pip.py"         # Package Installer for Python
+        rm "${PROJ_EXT}/get-pip.py"                                                         # clean PIP script
+        ${PYTHON_DIR//\{VERSION\}/$ver}/Scripts/pip install tox                             # test automation (by virtualenv) 
+        ${PYTHON_DIR//\{VERSION\}/$ver}/Scripts/pip install poetry                          # [1] build automation (by virtualenv, setuptools) 
+        ${PYTHON_DIR//\{VERSION\}/$ver}/Scripts/pip install pipreqs
+    done
+    # PVM checkup (negative)
+    if [[ ! -d "$PROJ_EXT" ]]; then
+        echo "Error: $(TEST_FAIL 'There is no '${PROJ_EXT}' found.')"
+        return
+    fi
+    # PVE setup
+    SETUP_PYTHON
+}
+function SETUP_PYTHON {
+    for ver in "${PYTHON_VERSIONS[@]}"; do
+        ver=$(echo "$ver" | awk -F. '{print $1 $2}')    # version modification e.g. "3.12.2" to "312"
+        export PATH="${PYTHON_DIR//\{VERSION\}/$ver}:${PATH}"
+        export PATH="${PYTHON_DIR//\{VERSION\}/$ver}/Scripts:${PATH}"
+    done
+    python --version
+    pip --version
+    poetry --version
+    tox --version
+    pipreqs --version
+}
+
+
+
+
+#
+
 
 ###############################################################################
 #                                   ?JAVA;
